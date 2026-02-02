49cfac715103572934fcddf6803074f21bc3486c
alexlam0206 <nok@pgmiv.com>
2026-02-02 22:43:09 +0800

    feat: add local JSON data storage  and /data endpoint

--- inky/backend/server.js
+++ inky/backend/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
   res.send('ok');
+});
+
+app.get('/data', (req, res) => {
+  const filePath = path.join(__dirname, 'data.json');
+  const data = fs.readFileSync(filePath, 'utf8');
+  res.json(JSON.parse(data));
 });
 
 app.listen(port, () => {
