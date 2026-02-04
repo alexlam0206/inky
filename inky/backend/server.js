a6b7587e678101fa99d49107939014114ffad2ff
alexlam0206 <nok@pgmiv.com>
2026-02-04 19:26:55 +0800

    refactor: separate e-ink render logic from server

--- inky/backend/server.js
+++ inky/backend/server.js
@@ -5,32 +5,18 @@ const { renderDashboard } = require('./render');
 const app = express();
 const port = 3001;
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('ok');
-});
-
-app.get('/data', (req, res) => {
-  const filePath = path.join(__dirname, 'data.json');
-  const data = fs.readFileSync(filePath, 'utf8');
-  res.json(JSON.parse(data));
-});
-
-app.post('/data', (req, res) => {
-  const filePath = path.join(__dirname, 'data.json');
-  const newData = req.body;
-  fs.writeFileSync(filePath, JSON.stringify(newData, null, 2));
-  res.json({ success: true });
-});
-
-app.get('/render', (req, res) => {
-  const filePath = path.join(__dirname, 'data.json');
-  const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-  const pngBuffer = renderDashboard(data);
-  
-  res.set('Content-Type', 'image/png');
-  res.send(pngBuffer);
+app.get('/render', async (req, res) => {
+  try {
+    const filePath = path.join(__dirname, 'data.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    const pngBuffer = await renderDashboard(data);
+    
+    res.set('Content-Type', 'image/png');
+    res.send(pngBuffer);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error rendering dashboard');
+  }
 });
 
 app.listen(port, () => {
